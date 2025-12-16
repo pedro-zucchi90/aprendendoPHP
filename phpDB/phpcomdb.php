1c0522c917075c441ea4c3d032e3874f73916cf3
pedro-zucchi90 <pedro@zucchi.dev.br>
2025-12-16 17:42:10 -0300

    feat: Initialize `database.json` with an empty array if it doesn't exist.

--- phpDB/phpcomdb.php
+++ phpDB/phpcomdb.php
@@ -1,1 +1,12 @@
 <?php
+
+$arquivo = "database.json";
+
+if (file_exists($arquivo)) {
+    $conteudo = file_get_contents($arquivo);
+} else {
+    $conteudo = "[]";
+    file_put_contents($arquivo, $conteudo);
+}
+
+$dados = json_decode($conteudo, true);
