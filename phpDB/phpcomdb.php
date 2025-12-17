4d5a0d42413e2a061243706f35db9fb8a3860bde
pedro-zucchi90 <pedro@zucchi.dev.br>
2025-12-17 08:46:11 -0300

    feat: Implement basic JSON file data storage for name and email.

--- phpDB/phpcomdb.php
+++ phpDB/phpcomdb.php
@@ -1,12 +1,27 @@
 <?php
+$nome = "NOME QUALQUER";
+$email = "EMAIL QUALQUER";
 
 $arquivo = "database.json";
+$conteudo = [];
 
 if (file_exists($arquivo)) {
     $conteudo = file_get_contents($arquivo);
+    $conteudo = json_decode($conteudo, true);
+    if (!is_array($conteudo)) {
+        $conteudo = [];
+    }
 } else {
-    $conteudo = "[]";
-    file_put_contents($arquivo, $conteudo);
+    $conteudo = [];
 }
 
-$dados = json_decode($conteudo, true);
+if ($nome && $email) {
+    $dados = ["nome" => $nome, "email" => $email];
+    $conteudo[] = $dados;
+
+    file_put_contents($arquivo, json_encode($conteudo, JSON_PRETTY_PRINT));
+} else {
+    echo "Nenhum dado foi inserido";
+}
+
+print_r($conteudo);
